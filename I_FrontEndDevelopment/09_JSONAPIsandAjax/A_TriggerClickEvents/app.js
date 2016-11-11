7b2a72b018d57d7d3130bd24da66c5af7758ab90
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-11 15:04:07 -0600

    Render Images from Data Sources

--- I_FrontEndDevelopment/09_JSONAPIsandAjax/A_TriggerClickEvents/app.js
+++ I_FrontEndDevelopment/09_JSONAPIsandAjax/A_TriggerClickEvents/app.js
@@ -3,7 +3,23 @@ console.clear();
 $(document).ready(function() {
  $("#getMessage").on("click", function() {
    $.getJSON("https://crossorigin.me/https://www.freecodecamp.com/json/cats.json", function(json) {
-      $(".message").html(JSON.stringify(json));
+     var html = "";
+
+     json.forEach(function(val) {
+
+       html += "<div class = 'cat'>";
+
+       // Only change code below this line.
+       html += "<img src = '" + val.imageLink + "' " + "alt='" + val.altText + "'>";
+
+
+       // Only change code above this line.
+
+       html += "</div>";
+
+     });
+
+        $(".message").html(html);
     });
  });
 });
