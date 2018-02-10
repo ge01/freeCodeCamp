58b8b8a1517a4ec8c76ff88046fcc1a9571a085e
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-09 21:24:44 -0600

    concatenating strings with the plus equals operator

--- I_FrontEndDevelopment/E_BasicJavaScript/27_ConcStrPlusEquals.js
+++ I_FrontEndDevelopment/E_BasicJavaScript/27_ConcStrPlusEquals.js
@@ -3,6 +3,5 @@ var ourStr = "I come first. ";
 ourStr += "I come second.";
 
 // Only change code below this line
-
 var myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.";
