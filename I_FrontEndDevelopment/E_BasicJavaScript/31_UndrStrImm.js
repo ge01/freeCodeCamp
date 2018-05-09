7eed27aa3f2ce8ce7f37bf8e4a3381a3586b7ac0
ge01 <gabriel.escobedo01@utrgv.edu>
2018-05-09 13:09:14 -0500

    Understand String Immutability

---
+++ I_FrontEndDevelopment/E_BasicJavaScript/31_UndrStrImm.js
@@ -0,0 +1,13 @@
+/*
+* Understand String Immutability
+* In JavaScript, String values are immutable, which means they cannot be alterd
+* once created.
+*/
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+//myStr[0] = "H"; // Fix Me
+myStr = "Hello World"; // Fix Me
